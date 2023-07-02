5935390b46a41bece915faaf597bb684442918ee
mikopratama54 <133556360+mikopratama54@users.noreply.github.com>
2023-07-02 19:40:31 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,14 +1,6 @@
+// toggle class active
 const navbarNav = document.querySelector(".navbar-nav");
-const hamburgerMenu = document.querySelector("#hamburger-menu");
-
-// Ketika hamburger-menu di klik
-hamburgerMenu.addEventListener("click", () => {
+// ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
-});
-
-// Klik di luar sidebar untuk menghilangkan navbar
-document.addEventListener("click", function (e) {
-  if (!hamburgerMenu.contains(e.target) && !navbarNav.contains(e.target)) {
-    navbarNav.classList.remove("active");
-  }
-});
+};
